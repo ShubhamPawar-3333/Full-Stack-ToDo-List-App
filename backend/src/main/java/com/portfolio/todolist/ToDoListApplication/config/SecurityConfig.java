800d07fb02679b135084076ee8f5a21b670692ef
ShubhamPawar-3333 <shubham.d.pawar.3333@gmail.com>
2025-07-14 19:42:04 +0530

    Add Security Configurations for application

---
+++ backend/src/main/java/com/portfolio/todolist/ToDoListApplication/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.portfolio.todolist.ToDoListApplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Spring Security configuration for JWT-based authentication.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter){
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    /**
+     * Configures HTTP security settings.
+     *
+     * @param http The HttpSecurity configuration.
+     * @return The configured SecurityFilterChain.
+     * @throws Exception If configuration fails.
+     */
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/auth/**", "/h2-console/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // Enable H2 console access
+        http.headers(header ->
+                header.frameOptions(frameOption -> frameOption.disable()));
+
+        return http.build();
+    }
+}
