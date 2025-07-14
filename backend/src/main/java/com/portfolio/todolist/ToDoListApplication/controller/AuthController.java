7ece16b72ca3eb04867f97570ccf20d83db90f00
ShubhamPawar-3333 <shubham.d.pawar.3333@gmail.com>
2025-07-14 22:17:07 +0530

    Add Authentication Controller for New user to register

---
+++ backend/src/main/java/com/portfolio/todolist/ToDoListApplication/controller/AuthController.java
@@ -0,0 +1,47 @@
+package com.portfolio.todolist.ToDoListApplication.controller;
+
+import com.portfolio.todolist.ToDoListApplication.dto.UserDTO;
+import com.portfolio.todolist.ToDoListApplication.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST controller for user authentication endpoints.
+ */
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService){
+        this.userService = userService;
+    }
+
+    /**
+     * Registers a new user.
+     *
+     * @param userDTO The user data.
+     * @return The registered username.
+     */
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody UserDTO userDTO){
+        String username = userService.registerUser(userDTO);
+        return ResponseEntity.ok(username);
+    }
+
+    /**
+     * Authenticates a user and returns a JWT token.
+     *
+     * @param userDTO The user credentials.
+     * @return The JWT token.
+     */
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserDTO userDTO){
+        String token = userService.loginUser(userDTO);
+        return ResponseEntity.ok(token);
+    }
+}
